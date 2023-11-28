3dcdb9a0b1ddbca42bc19e0eeefb8591b7aaecbd
Jonah Bodnar <129788519+JonahETB@users.noreply.github.com>
2023-11-28 10:36:31 -0600

    commit final

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var gradeObject = {
     elementInput: document.getElementById("grade"), // This line gets an input element with the id "grade".
     studentCount: 0, // A variable to keep track of the number of students added. This is improtant when removing grades.
     setGrade: function () {
-        let inputValue = this.elementInput.value;
-        if (inputValue !== 0){
-            this.grades.push(+inputValue); // Adds the grade to the 'grades' array.
-            this.printIndividual(+inputValue);
+        if (this.elementInput.value != 0 && this.elementInput.value != null){
+            this.grades.push(+this.elementInput.value); // Adds the grade to the 'grades' array.
+            this.printIndividual(+this.elementInput.value);
+        } else {
+            alert('Please enter a valid number above 0');
         }
         this.elementInput.value = "";
     },
@@ -21,22 +22,25 @@ var gradeObject = {
         return {high, low};
     },
     calcPassedFailed: function () {
-        const aboveThreshold = (this.grades.filter(num => num > 50)).length, belowThreshold = (this.grades.filter(num => num <= 50)).length;
-        return{aboveThreshold, belowThreshold}; //creates an object out of the function with the grades filtered into the length of each array one above 50 and the other below
+        const aboveThreshold = (this.grades.filter(num => num >= 50)).length, belowThreshold = (this.grades.filter(num => num < 50)).length;
+        return{aboveThreshold, belowThreshold};
     },
     calcOutputResult: function () {
-        let passFail = this.calcPassedFailed(), highLow = this.calcHighLow(), cal = this.calcAverage();
-        let person1 = passFail.aboveThreshold === 1 ? "person" : "people", person2 = passFail.belowThreshold === 1 ? "person" : "people";
-        if(this.grades.length === 0 || cal.sum === 0) {
+        const responses = [ this.calcPassedFailed(), this.calcHighLow(), this.calcAverage()];
+        let person = [responses[0].aboveThreshold === 1 ? "person" : "people", responses[0].belowThreshold === 1 ? "person" : "people"];
+        if(this.grades.length === 0 || responses[2].sum === 0) {
             alert('No students have been added yet or the sum of the grades must be above 0');
             return;
         }
-        document.getElementById("average").innerHTML = `You entered ${this.grades.length} grades<br>The total of all the grades is ${cal.sum}<br> The grade average is ${cal.average}<br>The highest score is ${highLow.high}<br> The lowest score is ${highLow.low}<br>${passFail.aboveThreshold} ${person1} passed the test.<br>${passFail.belowThreshold} ${person2} failed the test.<br>`;
+        document.getElementById("average").innerHTML = `You entered ${this.grades.length} grades<br>The total of all the grades is 
+        ${responses[2].sum}<br> The grade average is ${responses[2].average}<br>The highest score is ${responses[1].high}<br> The lowest score is ${responses[1].low}<br>
+        ${responses[0].aboveThreshold} ${person[0]} passed the test.<br>${responses[0].belowThreshold} ${person[1]} failed the test.<br>`;
     },
     printIndividual: function (num) {
         this.studentCount++;
-        let text = num >= 50 ? document.createTextNode(`Student ${this.studentCount} passed with a grade of ${num}% `) : document.createTextNode(`Student ${this.studentCount} failed with a grade  of ${num}% `);
-        const p = document.createElement("p"); // Creates a new paragraph element.
+        let passFailText = num >= 50 ? "passed" : "failed";
+        let text = document.createTextNode(`Student ${this.studentCount} ${passFailText} with a grade of ${num}% `) 
+        const p = document.createElement("p"); // Creates a new paragraph element.S
                 p.id = `text${this.studentCount}`; // Assigns an id to the paragraph element.
                 p.appendChild(text); // Appends the text node to the paragraph element.
         const delButton = document.createElement("input");
@@ -57,8 +61,7 @@ var gradeObject = {
     },
     checkGrade: function () {
         //checks how big the input number is and it also check the length if anything is out of the ordinary is fixes it
-        let inputValue = this.elementInput.value >= 100 ? this.elementInput.value = 100 : this.elementInput.value = this.elementInput.value;
-        let checkLength = (this.elementInput.value).length >= 6 ? this.elementInput.value = (this.elementInput.value).slice(0, -1) : this.elementInput.value = this.elementInput.value;
+        let checkValue = this.elementInput.value >= 100 ? this.elementInput.value = 100 : this.elementInput.value = this.elementInput.value;
     }
 };
 
